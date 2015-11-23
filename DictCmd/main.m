5806a4c785820ec28411f07b67ece4d4f093809b
Zach <zach_41@163.com>
2015-11-23 10:35:50 +0800

    Initial Commit

---
+++ DictCmd/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  DictCmd
+//
+//  Created by ZachZhang on 15/11/23.
+//  Copyright © 2015年 ZachZhang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
